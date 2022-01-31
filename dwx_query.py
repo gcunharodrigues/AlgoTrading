f29107681f554f654a80d6e15bc35f4796f473ab
guicr <gcunharodrigues@yahoo.es>
2022-01-30 23:04:35 -0300

    Historic data query done

--- dwx_query.py
+++ dwx_query.py
@@ -14,24 +14,36 @@ from api.dwx_client import dwx_client
 
 def send_historic_data(MT4_files_dir, symbol, timeframe, start, end):
     dwx = dwx_client(metatrader_dir_path=MT4_files_dir)
-    sleep(0.5)
+    sleep(1)
     dwx.get_historic_data(symbol, timeframe, start, end)
-    sleep(0.5)
+    sleep(10)
     for st in dwx.historic_data.keys():
         on_historic_data(symbol, timeframe, dwx.historic_data[st])
-        print(dwx.historic_data[st])
+        historic_data_list = []
+        for k, v in dwx.historic_data[st].items():
+            historic_time_list = []
+            historic_time_list.append(k)
+            for data in v:
+                historic_time_list.append(dwx.historic_data[st][k][data])
+            
+            historic_data_list.append(historic_time_list)
 
+    return historic_data_list
+                
+# Function to print historic data information.
 def on_historic_data(symbol, time_frame, data):
         
     # you can also access the historic data via self.dwx.historic_data. 
     print('historic_data:', symbol, time_frame, f'{len(data)} bars')
 
 
-MT4_files_dir = 'C:/Users/guicr/AppData/Roaming/MetaQuotes/Terminal/'\
-                'D0E8209F77C8CF37AD8BF550E51FF075/MQL5/Files/'
-symbol = 'WIN$'
-timeframe = 'H1'
-start = datetime(2020, 1, 1).timestamp()
-end = datetime(2020, 1, 3).timestamp()
+# MT4_files_dir = 'C:/Users/guicr/AppData/Roaming/MetaQuotes/Terminal/'\
+#                 'D0E8209F77C8CF37AD8BF550E51FF075/MQL5/Files/'
+# MT4_files_dir = '/home/gcunharodrigues/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Files/'
+# symbol = 'WIN$'
+# timeframe = 'H4'
+# start = datetime(2020, 1, 1).timestamp()
+# end = datetime(2020, 1, 3).timestamp()
 
-send_historic_data(MT4_files_dir, symbol, timeframe, start, end)
+# historic_data = send_historic_data(MT4_files_dir, symbol, timeframe, start, end)
+# print(historic_data)
